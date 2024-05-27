8b4d94a59aa70a8ddf59d78080896bd4d7924bb3
callummiles <cwmiles18@gmail.com>
2024-05-27 18:32:14 -0500

    removed event data collection for now

--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -11,10 +11,7 @@ export const getDataByMatchId = async (req, res) => {
     }
 
     const shots = matchData.content.shotmap.shots;
-    const events = matchData.content.matchFacts.events.events;
     const halfs = matchData.header.status.halfs;
-
-    console.log(events);
 
     const shotsResult = await Match.findOneAndUpdate(
       { id: matchId },
@@ -26,20 +23,11 @@ export const getDataByMatchId = async (req, res) => {
       { $set: { halfs: halfs } }
     );
 
-    const eventsResult = await Match.findOneAndUpdate(
-      { id: matchId },
-      { $set: { events: events } }
-    );
-
     if (!shotsResult) {
       return res.status(404).json({ error: 'Match not found.' });
     }
 
     if (!halfsResult) {
-      return res.status(404).json({ error: 'Match not found.' });
-    }
-
-    if (!eventsResult) {
       return res.status(404).json({ error: 'Match not found.' });
     }
 
